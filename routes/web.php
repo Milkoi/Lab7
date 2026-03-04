c7ba698e307b034408b62bb968877f61748b7e61
Milkoi <minhlequan2005@gmail.com>
2026-03-04 09:45:47 +0700

    Lab7

---
+++ routes/web.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', [HomeController::class, 'index']);
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'list']);
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/lienhe', function () {
+    return view('lienhe');
+});
+
+Route::get('/bang-cuu-chuong/{n}', [HomeController::class, 'multiplication']);
+
+
+Route::get('/about', function () {
+    return "Họ tên: Nguyễn Văn Công - Lớp: D18CNPM2 - MSSV: 23810310128";
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/tong/{a}/{b}', function ($a, $b) {
+    $tong = $a + $b;
+    return "Tổng của $a và $b là: $tong";
+});
+Route::get('/sinh-vien/{name}/{age?}', function ($name, $age = 20) {
+    return "Sinh viên: $name - Tuổi: $age";
+});
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return "Chào mừng Admin";
+    });
+
+    Route::get('/users', function () {
+        return "Danh sách người dùng";
+    });
+
+});
+Route::get('/check-date/{day}/{month}/{year}', function ($day, $month, $year) {
+    return "Ngày hợp lệ: $day/$month/$year";
+})->where([
+    'day' => '^(0?[1-9]|[12][0-9]|3[01])$',
+    'month' => '^(0?[1-9]|1[0-2])$',
+    'year' => '^[0-9]{4}$'
+]);
+
+
